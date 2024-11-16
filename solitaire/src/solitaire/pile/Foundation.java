5bfcaa521d8cb208c2499d38d162681d1bdcbad1
Abrizja <abrizja101@gmail.com>
2024-11-16 23:07:25 +0100

    Add files via upload

---
+++ solitaire/src/solitaire/pile/Foundation.java
@@ -0,0 +1,26 @@
+package solitaire.pile;
+
+import java.awt.event.ContainerAdapter;
+import java.awt.event.ContainerEvent;
+import solitaire.card.Card;
+import static solitaire.card.Card.CARD_WIDTH;
+
+final public class Foundation extends Pile {
+    
+    public Foundation(String suit) {
+        super(suit);
+        
+        setFont(getFont().deriveFont((float) CARD_WIDTH));
+        addContainerListener(new ContainerAdapter() {
+            @Override
+            public void componentAdded(ContainerEvent e) {
+                e.getChild().setLocation(0, 0);
+            }
+        });
+    }
+    
+    public Card.Rank nextRank() {
+        return Card.Rank.values()[getComponentCount()];
+    }
+    
+}
